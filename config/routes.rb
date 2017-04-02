003c9ea7884235c76f03013bee22c9bb1a67dcb5
Jaime Echeverría <rpggrenade@gmail.com>
2017-04-02 20:45:40 -0300

    Laboratorio 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :enrollments
   resources :courses
   resources :people
+
+  root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
